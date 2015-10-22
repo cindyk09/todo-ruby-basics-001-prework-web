9d9ac1673988826f8145713b95a8b1bad48828a2
cindyk09 <cindy_kim88@hotmail.com>
2015-10-21 22:02:27 -0400

    Ruby Basics Review

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
-def assign_variable(value)
+def assign_variable(name)
+  value = name
+end
+
+def argue(arg)
+  arg
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting(name,greeting)
 
 end
 
-def return_a_value
+def return_a_value(phrase = "Nice")
+  phrase
 
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase = "expert")
+  phrase
 end
 
-def pizza_party
+def pizza_party(phrase = "cheese")
+  phrase
+end
 
+def pizza_party(topping = "cheese")
+  topping
 end
